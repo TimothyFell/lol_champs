1accf3b12f2b5fac5adbcda914dff3fa0f24cf40
TimothyFell <timothy.fell846@gmail.com>
2019-02-26 08:47:33 -0700

    Add route for user login and registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get '/', to: 'champions#index', as: 'champions'
+  get '/', to: 'champions#index', as: 'root'
 
   get '/login', to: 'sessions#new', as:'login'
+  get '/register', to: 'users#new', as: 'register'
+  post '/register', to: 'users#create', as: 'users'
 
   get '/champions/:name', to: 'champions#show', as: 'champion'
   post '/favorites/:name', to: 'favorites#create', as: 'favorites'
