32e408bd9570ac0edbd3537bda5f2efdc9e6cf40
TimothyFell <timothy.fell846@gmail.com>
2019-02-26 11:06:46 -0700

    Add user creation to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,24 @@
 class UsersController < ApplicationController
 
   def new
-    @user = User.new
+    if current_user
+      flash[:info] = 'You are already logged in'
+      redirect_to root_path
+    else
+      @user = User.new
+    end
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to(root_path)
+    if current_user
+      flash[:info] = 'You are already logged in'
+      redirect_to root_path
+    else
+      user = User.new(user_params)
+      if user.save
+        session[:user_id] = user.id
+        redirect_to(root_path)
+      end
     end
   end
 
