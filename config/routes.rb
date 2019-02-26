6df468788063b4d9e026adb05ecad83c1301e117
TimothyFell <timothy.fell846@gmail.com>
2019-02-26 07:40:15 -0700

    Add routes for favorites and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get '/', to: 'champions#index', as: 'champions'
 
+  get '/login', to: 'sessions#new', as:'login'
+
   get '/champions/:name', to: 'champions#show', as: 'champion'
+  post '/favorites/:name', to: 'favorites#create', as: 'favorites'
 end
