712d03deba7e6b9437c6ae7c08fc244d44e75476
TimothyFell <timothy.fell846@gmail.com>
2019-02-26 10:32:29 -0700

    Add route for session post

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   get '/', to: 'champions#index', as: 'champions'
   get '/', to: 'champions#index', as: 'root'
 
-  get '/login', to: 'sessions#new', as:'login'
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
   get '/register', to: 'users#new', as: 'register'
-  post '/register', to: 'users#create', as: 'users'
+  post '/register', to: 'users#create', as:'users'
 
   get '/champions/:name', to: 'champions#show', as: 'champion'
   post '/favorites/:name', to: 'favorites#create', as: 'favorites'
