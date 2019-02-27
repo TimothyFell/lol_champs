565b93767147dd8970fec6409f83d996c879259f
TimothyFell <timothy.fell846@gmail.com>
2019-02-26 17:34:46 -0700

    Write helper method to determine if a champion is a favorite of a user

--- app/models/champion.rb
+++ app/models/champion.rb
@@ -1,4 +1,12 @@
 class Champion < ApplicationRecord
   has_many :favorites
   has_many :users, through: :favorites
+
+  def favorite?(user)
+    if user
+      user.favorites.find_by(champion_id: self.id).nil?
+    else
+      return true
+    end
+  end
 end
