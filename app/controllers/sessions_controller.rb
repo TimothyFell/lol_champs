1aafc891c733b309c0bbbec34dde68c35567ef71
TimothyFell <timothy.fell846@gmail.com>
2019-02-26 17:58:04 -0700

    Add destroy action to session controller to remove user_id from the session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    flash[:info] = 'You are logged out'
+    flash[:info] = 'You are logged out.'
     redirect_to root_path
   end
 
